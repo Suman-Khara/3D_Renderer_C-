28dcabeebfbc52d619d7d23920397e8614af59d4
Suman <sumankhara12d@gmail.com>
2024-08-04 17:50:26 +0530

    Smoothening by Generating Pixels with Multiple Samples

--- colour.h
+++ colour.h
@@ -2,14 +2,24 @@
 #define COLOUR_H
 
 #include "vec3.h"
+#include "interval.h"
 
 using colour = vec3;
 
-inline void write_colour(std::ostream &out, colour pixel_colour)
+inline void write_colour(std::ostream &out, const colour &pixel_colour)
 {
-    out << static_cast<int>(255.999 * pixel_colour.x()) << ' '
-        << static_cast<int>(255.999 * pixel_colour.y()) << ' '
-        << static_cast<int>(255.999 * pixel_colour.z()) << '\n';
+    auto r = pixel_colour.x();
+    auto g = pixel_colour.y();
+    auto b = pixel_colour.z();
+
+    // Translate the [0,1] component values to the byte range [0,255].
+    static const interval intensity(0.000, 0.999);
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
+
+    // Write out the pixel colour components.
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
 
 #endif // COLOUR_H
